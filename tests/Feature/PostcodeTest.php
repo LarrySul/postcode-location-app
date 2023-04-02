589e23cbb5af0767674b0df04c240f60729b88c5
Olanrewaju <olanrewaju@seamlesshr.com>
2023-04-03 00:07:46 +0100

    update feature test

--- tests/Feature/PostcodeTest.php
+++ tests/Feature/PostcodeTest.php
@@ -6,35 +6,38 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Postcode;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+// use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PostcodeTest extends TestCase
 {
-    use DatabaseTransactions;
+    use  RefreshDatabase;
     public function setUp(): void
     {
         parent::setUp();
-        Postcode::factory()->count(10)->create();
+        Postcode::factory()->count(100)->create();
     }
 
     public function test_get_all_postcodes_with_filter()
     {
         $query = '';
-        $response = $this->get('/api/v1/postcode/get-all-postcodes?usertype=1&q='. $query);
+        $response = $this->get('/api/v1/postcode/get-all-postcodes?usertype=1&q='.$query);
+        
         $response->assertStatus(200);
         $jsonResponse = json_decode($response->getContent(), true);
-        $this->assertArrayHasKey('postcodes', $jsonResponse);
-        $this->assertNotEmpty($jsonResponse['postcodes']);
+        $this->assertArrayHasKey('status', $jsonResponse);
+        $this->assertArrayHasKey('data', $jsonResponse);
+        $this->assertArrayHasKey('message', $jsonResponse);
+        $this->assertNotEmpty($jsonResponse['data']);
     }
 
     public function test_returns_nearby_postcodes()
     {
         $lat = 10.5074;
-        $long = -2.1278;
-        $response = $this->get('/api/v1/postcode/get-nearby-postcodes?usertype=1&lat=' . $lat . '&long=' . $long . '&distance=100');
-        $response->assertStatus(200);
+        $long = -1.1278;
+        $response = $this->get('/api/v1/postcode/get-nearby-postcodes?usertype=1&lat=' . $lat . '&long=' . $long . '&distance=10');
         $jsonResponse = json_decode($response->getContent(), true);
-        $this->assertArrayHasKey('postcodes', $jsonResponse);
-        $this->assertNotEmpty($jsonResponse['postcodes']);
+        $this->assertArrayHasKey('status', $jsonResponse);
+        $this->assertArrayHasKey('data', $jsonResponse);
+        $this->assertArrayHasKey('message', $jsonResponse);
     }
 }
